e69fbe287efa0c673f10728ec7fc846ebf8cecda
linus mutugi mate <mlynux@gmail.com>
2022-08-04 18:53:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+
+function writeCards(array,event) {
+    let newArray = [];
+   for(let i = 0; i < array.length; i++) {
+   
+     const gratitude = `Thank you, ${array[i]}, for the wonderful ${event} gift!`
+     newArray.push(gratitude)
+   
+}
+   return newArray
+}
+ 
+ function countDown(){
+    let numCountdown = 11;
+    while(numCountdown--){
+        console.log(numCountdown)
+    }
+ }
